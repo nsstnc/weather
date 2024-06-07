4daa35519d575974602a4714f6aadd10dda6b669
nemopss <alexey_gladilin@mail.ru>
2024-06-08 00:27:25 +0300

    FIX: changed port from 222 to 2222 and replaced xdg-open with open

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ source venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 
-python manage.py runserver 222 &
+python manage.py runserver 2222 &
 
-xdg-open http://127.0.0.1:222/
+open http://127.0.0.1:2222/
 
 read -p "Press any key to continue..."
